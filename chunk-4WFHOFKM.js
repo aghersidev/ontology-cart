fabaea21f185bb1775f4e73438e68e5bc7cf04c1
aghersidev <aghersidev@users.noreply.github.com>
2026-01-14 05:21:17 +0000

    deploy: b1197fd00b2339d44d1b7416dd00be6a04ca8d0d

--- chunk-4WFHOFKM.js
+++
@@ -1,1 +0,0 @@
-import{Na as n,Oa as m,Ya as p,za as t}from"./chunk-M3VX3GME.js";var a=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-home"]],decls:2,vars:0,template:function(e,i){e&1&&(n(0,"p"),p(1,"home works!"),m())},encapsulation:2})};export{a as HomeComponent};
